0303595cb21647b203ae9069a5191cbd4ad0f865
Adib Saikali <asaikali@gmail.com>
2020-08-23 22:05:34 -0400

    Project Skeleton

---
+++ src/main/java/com/example/cryptohash/CryptoHashApplication.java
@@ -0,0 +1,12 @@
+package com.example.cryptohash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoHashApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CryptoHashApplication.class, args);
+  }
+}
